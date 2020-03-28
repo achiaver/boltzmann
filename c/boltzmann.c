c1bc30792a1cbd7e7323c0f412e0b2a48b2c86b3
Artur C. Grover <artur.turbas@gmail.com>
2020-03-28 19:26:50 -0300

    Modified code to display proper error message

--- c/boltzmann.c
+++ c/boltzmann.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 #include "boltzmann.h"
 #include "stats_functions.h"
 
@@ -16,59 +17,82 @@
 
 // PRECISO ACABAR DE ARRUMAR AS FUNÇÕES PARA CRIAR CADA CAMADA E CADA NODE.
 
-struct layer* create_layer(struct parameters param) {
-    struct layer* layer = malloc(sizeof (struct layer) * (param.num_layers));
-    if (layer != NULL) {
-        for (int i = 0; i < param.num_layers; i++) {
-            printf("i - %d \n", i);
-            layer[i].num_nodes = param.num_nodes_array[i];
-            layer[i].node = malloc(sizeof (struct layer) * (layer[i].num_nodes));
-            if (layer[i].node != NULL) {
-                for (int j = 0; j < layer[i].num_nodes; j++) {
-                    printf("i - %d \t j - %d \n", i, j);
-                    layer[i].node[j].activation = 0;
-                    layer[i].node[j].bias = 0;
-                    layer[i].node[j].h_in = 0;
-//                    if (&layer[i+1] != NULL) {AQUI PRECISO PENSAR EM COMO CONSIDERAR MAIS DE DUAS CAMADAS
-//                                              ACHO QUE A OPCAO MAIS FACIL VAI SER USANDO DOIS VETORES DE PESOS
-//                                              UM PARA OS PESOS DA ENTRADA W^(I) E UM PARA PESOS DA SAIDA W^(O)}
-//                  NAO ESTA ENTRANDO NESSE IF...???
-//                    i = 0;
-                    if (i == 0) {
-                        layer[i].node[j].weight = malloc(sizeof (double) * (layer[i+1].num_nodes));
-                        if (layer[i].node[j].weight != NULL) {
-                            for (int k = 0; k < layer[i+1].num_nodes; k++) {
-                                printf("i - %d \t j - %d \t k - %d \n", i, j, k);
-                                layer[i].node[j].weight[k] = 2.2; //(double) (10 * (j+1)) + (k+1);
-                                printf("%f \n", layer[i].node[j].weight[k]);
-                            }
-                        }
-                    }
-                    if (i > 0) {
-                        layer[i].node[j].weight = malloc(sizeof (double) * (layer[i-1].num_nodes));
-                        if (layer[i].node[j].weight != NULL) {
-                            for (int k = 0; k < layer[i-1].num_nodes; k++) {
-                                 printf("i - %d \t j - %d \t k - %d \n", i, j, k);
-                                 layer[i].node[j].weight[k] = 42; //layer[i-1].node[k].weight[j];
-                                 printf("%f \n", layer[i].node[j].weight[k]);
-                            }
-                        }
-                    }
-                    }
+struct layer * create_layer(struct parameters parameters_bm) {
+    struct layer * layer_bm = malloc(sizeof (*layer_bm) * (parameters_bm.num_layers));
+    if (!layer_bm) 
+    {
+        perror("create_layer: malloc: layer: %s \n", strerror(errno));
+        exit(2);
+    }
+
+    for (int i = 0; i < parameters_bm.num_layers; i++) 
+    {
+        printf("i - %d \n", i);
+        layer_bm[i].num_nodes = parameters_bm.num_nodes_array[i];
+        layer_bm[i].node = malloc(sizeof (struct node) * (layer_bm[i].num_nodes));
+        if (!layer_bm[i].node) 
+        {
+            perror("create_layer: malloc: node: %s\n", strerror(errno));
+            exit(2);
+        }
+
+        for (int j = 0; j < layer_bm[i].num_nodes; j++) 
+        {
+            printf("i - %d \t j - %d \n", i, j);
+            layer_bm[i].node[j].activation = 0;
+            layer_bm[i].node[j].bias = 0;
+            layer_bm[i].node[j].h_in = 0;
+//            if (&layer[i+1] != NULL) {AQUI PRECISO PENSAR EM COMO CONSIDERAR MAIS DE DUAS CAMADAS
+//                                      ACHO QUE A OPCAO MAIS FACIL VAI SER USANDO DOIS VETORES DE PESOS
+//                                      UM PARA OS PESOS DA ENTRADA W^(I) E UM PARA PESOS DA SAIDA W^(O)}
+//          NAO ESTA ENTRANDO NESSE IF...???
+//            i = 0;
+            if (i == 0) 
+            {
+                layer_bm[i].node[j].weight = malloc(sizeof (double) * (layer_bm[i+1].num_nodes));
+                if (!layer_bm[i].node[j].weight)
+                {
+                    perror("create_layer: malloc: node %d: weight: %s \n", j,  strerror(errno));
+                    exit(2);
                 }
-            printf("\n");
+
+                for (int k = 0; k < layer_bm[i+1].num_nodes; k++) 
+                {
+                    printf("i - %d \t j - %d \t k - %d \n", i, j, k);
+                    layer_bm[i].node[j].weight[k] = 2.2; //(double) (10 * (j+1)) + (k+1);
+                    printf("%f \n", layer_bm[i].node[j].weight[k]);
+                }
+            } else 
+            {
+                layer_bm[i].node[j].weight = malloc(sizeof (double) * (layer_bm[i-1].num_nodes));
+                if (!layer_bm[i].node[j].weight)
+                {
+                    perror("create_layer: malloc: node %d: weight: %s\n", j, strerror(errno));
+                    exit(2);
+                }
+
+                for (int k = ; k < layer_bm[i-1].num_nodes; k++) 
+                {
+                    printf("i - %d \t j - %d \t k - %d \n", i, j, k);
+                    layer_bm[i].node[j].weight[k] = 42; //layer[i-1].node[k].weight[j];
+                    printf("%f \n", layer_bm[i].node[j].weight[k]);
+                }
+            }
         }
-        }
-    return layer;
+    }
+    printf("\n");
+    return layer_bm;
 }
 
-struct network* create_network(struct parameters param) {
-    struct network* network = malloc(sizeof (struct network));
-    if (network != NULL) {
-        network->num_layers = param.num_layers;
-        network->layer = create_layer(param);
+struct network * create_network(struct parameters parameters_bm) {
+    struct network * network_bm = malloc(sizeof (*network_bm));
+    if (!network_bm) {
+        perror("create_network: malloc: network: %s \n", strerror(errno));
+        exit(2);
     }
-    return network;
+    network_bm->num_layers = parameters_bm.num_layers;
+    network_bm->layer = create_layer(parameters_bm);
+    return network_bm;
 }
 
 
@@ -85,12 +109,12 @@ int main(int argc, char *argv[])
 //        .maxepochs = 0
 //    };
 
-    struct parameters param;
-    input_parameters(&param);
-    print_parameters(param);
+    struct parameters parameters_bm;
+    input_parameters(&parameters_bm);
+    print_parameters(parameters_bm);
 
-    struct network* network = create_network(param);
-    print_network_status(*network);
+    struct network * network_bm = create_network(parameters_bm);
+    print_network_status(*network_bm);
 
 
 
