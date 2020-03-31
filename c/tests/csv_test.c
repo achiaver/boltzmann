d4074d42ecd7398877962fed2f3c62daf2caf56d
Artur C. Grover <artur.turbas@gmail.com>
2020-03-31 10:39:10 -0300

    testing read csv methods

---
+++ c/tests/csv_test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ";"); tok && *tok; tok = strtok(NULL, ";\n"))
+    {
+        printf("%s \n", tok);
+        if (!--num)
+        {
+            printf("%d \n", num);
+            return tok;
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    FILE* stream = fopen("read_csv_test_input.csv", "r");
+
+    char line[1024];
+    while (fgets(line, 1024, stream))
+    {
+        char* tmp = strdup(line);
+        printf("Field 3 would be %s\n", getfield(tmp, 10));
+        // NOTE strtok clobbers tmp
+        free(tmp);
+    }
+}
