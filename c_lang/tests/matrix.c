e20d74aee75496bce252fa006cd23df90bf0ff4f
Artur C. Grover <artur.turbas@gmail.com>
2020-04-07 10:59:55 -0300

    matrix.c has functions to do multiplication, addition and multiplication by scalar of matrix

--- c_lang/tests/matrix.c
+++ c_lang/tests/matrix.c
@@ -25,7 +25,7 @@ matrix_create( size_t rows, size_t cols)
     }
 
     return m;
-} /* matrix_create */
+} /* end of matrix_create */
 
 
 void
@@ -35,7 +35,7 @@ matrix_destroy(struct matrix *m)
         free(m->elem);
         free(m);
     }
-} /* matrix_destroy */
+} /* end of matrix_destroy */
 
 
 void
@@ -43,52 +43,52 @@ matrix_zero(struct matrix *m)
 {
     for (size_t e = 0; e < (m->rows * m->cols); e++)
         m->elem[e] = 0.;
-} /* matrix_zero */
+} /* end of matrix_zero */
 
 
 void
 matrix_print(struct matrix *m)
 {
-    for (size_t row = 0; row < m->rows; row++) 
+    for (size_t row = 0; row < m->rows; row++)
     {
-        for( size_t col = 0; col < m->cols; col++) 
+        for( size_t col = 0; col < m->cols; col++)
         {
             printf("%f, \t", matrix_get(m, row, col));
         }
         printf("\n");
     }
-} /* matrix_print */
+} /* end of matrix_print */
 
 
 void
 matrix_print_line(struct matrix *m)
 {
-    for (size_t row = 0; row < m->rows; row++) 
-        for( size_t col = 0; col < m->cols; col++) 
+    for (size_t row = 0; row < m->rows; row++)
+        for( size_t col = 0; col < m->cols; col++)
             printf("%f, \t", matrix_get(m, row, col));
     printf("\n");
-} /* matrix_print_line */
+} /* end of matrix_print_line */
 
 
 void
 matrix_shape(struct matrix *m)
 {
     printf("[%zu, %zu]\n", m->rows, m->cols);
-} /* matrix_shape */
+} /* end of matrix_shape */
 
 
 void
 matrix_set(struct matrix *m, size_t row, size_t col, double value)
 {
     m->elem[(row*m->cols) + col] = value;
-} /* matrix_set */
+} /* enf of matrix_set */
 
 
 double
 matrix_get(struct matrix *m, size_t row, size_t col)
 {
     return m->elem[(row*m->cols) + col];
-} /* matrix_get */
+} /* end of matrix_get */
 
 
 struct matrix *
@@ -97,7 +97,7 @@ matrix_read_data (char * filename, size_t rows, size_t cols)
     struct matrix * m_data = matrix_create(rows, cols);
 
     char buffer[1024];
-    int row = 0;
+    int row = 0, col = 0;
 
     FILE * fp = fopen(filename, "r");
 
@@ -110,19 +110,18 @@ matrix_read_data (char * filename, size_t rows, size_t cols)
     while (fgets(buffer, sizeof(buffer), fp) && (row < rows))
     {
         char *line = strdup(buffer);
-        int col;
         const char * data;
 
-        for (data = strtok(buffer, ","); data && *data; col++, data = strtok(NULL, ",\n"))
+        for (data = strtok(buffer, ","), col = 0; data && *data && (col < cols); col++, data = strtok(NULL, ",\n"))
         {
             matrix_set(m_data, row, col, atof(data));
         }
+        row++;
         free(line);
     }
-
     fclose(fp);
     return m_data;
-} /* matrix_read_data */
+} /* end of matrix_read_data */
 
 
 struct matrix *
@@ -136,7 +135,7 @@ matrix_sum (struct matrix * A, struct matrix * B)
 
     struct matrix * m_sum = matrix_create(A->rows, A->cols);
 
-    for (int row = 0; row < A->rows; row++) 
+    for (int row = 0; row < A->rows; row++)
     {
         for (int col = 0; col < A->cols; col++)
         {
@@ -145,9 +144,9 @@ matrix_sum (struct matrix * A, struct matrix * B)
     }
 
     return m_sum;
-} /* matrix_sum */
+} /* end of matrix_sum */
 
-// PENSAR EM COMO ESCREVER A MULTIPLICACAO DE MATRIZES USANDO GET, SET E SUM 
+
 struct matrix *
 matrix_product (struct matrix * A, struct matrix * B)
 {
@@ -160,32 +159,75 @@ matrix_product (struct matrix * A, struct matrix * B)
     struct matrix * m_product = matrix_create(A->rows, B->cols);
     matrix_zero(m_product);
 
+    double elem1 = 0., elem2 = 0.;
+
     for (int row = 0; row < A->rows; row++)
     {
         for (int col1 = 0; col1 < B->cols; col1++)
         {
-            for (int col2 = 0; col2 > A->cols; col2++)
+            for (int col2 = 0; col2 < A->cols; col2++)
             {
-                matrix_set(m_product, row, col, )
+                elem1 = matrix_get(m_product, row, col1);
+                elem2 = matrix_get(A, row, col2) * matrix_get(B, col2, col1);
+                matrix_set(m_product, row, col1, elem1 + elem2);
             }
         }
     }
 
     return m_product;
-}
+} /* end of matrix_product */
+
+
+struct matrix *
+matrix_product_scalar (struct matrix * A, double scalar)
+{
+    struct matrix * m_prod_scalar = matrix_create(A->rows, A->cols);
+
+    for (int row = 0; row < A->rows; row++)
+    {
+        for (int col = 0; col < A->cols; col++)
+        {
+            matrix_set(m_prod_scalar, row, col, matrix_get(A, row, col) * scalar);
+        }
+    }
+
+    return m_prod_scalar;
+} /* end matrix_product_scalar */
 
 
 int main (int args, char * argv[]) {
 
 
-    struct matrix * data = matrix_read_data("../dataset/example.csv", 4, 5);
-    matrix_shape(data);
+    struct matrix * data1 = matrix_read_data("../dataset/example_1.csv", 3, 3);
+    struct matrix * data2 = matrix_read_data("../dataset/example_2.csv", 3, 5);
 
-    matrix_print(data);
+    matrix_shape(data1);
+    matrix_print(data1);
     printf("\n\n");
 
-    struct matrix * C = matrix_sum(data, data);
+    matrix_shape(data2);
+    matrix_print(data2);
+    printf("\n\n");
+
+
+    struct matrix * C = matrix_sum(data1, data1);
+    printf("Sum of data matrix\n");
     matrix_print(C);
+
+    printf("\n");
+    struct matrix * D = matrix_product(data1, data1);
+    printf("Product of data matrix with itself\n");
+    matrix_print(D);
+
+    printf("\n");
+    struct matrix * E = matrix_product_scalar(data1, 2.);
+    printf("Product of data matrix by scalar\n");
+    matrix_print(E);
+
+    printf("\n");
+    struct matrix * F = matrix_product(data1, data2);
+    printf("Product of data1 * data2\n");
+    matrix_print(F);
 
 //    double res;
 //    size_t cols = 3, rows = 4;
@@ -214,8 +256,12 @@ int main (int args, char * argv[]) {
 //    printf("\n");
 //    matrix_destroy(my_matrix);
 
-    matrix_destroy(data);
-
+    matrix_destroy(data1);
+    matrix_destroy(data2);
+    matrix_destroy(C);
+    matrix_destroy(D);
+    matrix_destroy(E);
+    matrix_destroy(F);
 
     return 0;
 }
