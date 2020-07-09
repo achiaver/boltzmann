4a2789dca9d862eb202ab0dffabe0dc2f4adfb13
Artur Chiaperini Grover <artur.turbas@gmail.com>
2020-07-09 16:08:09 -0300

    something has been changed

--- python/make_preprocessing.py
+++ python/make_preprocessing.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
+import os
 import numpy as np
 
 num_examples = 2
-num_times = 30
+num_times = 40
 
 num_layers = 2
 num_hidden = 4
@@ -14,7 +15,7 @@ epsilonvb = 0.01
 epsilonhb = 0.01
 
 def add_examples (data, num_examples, num_visible):
-    return np.concatenate((data, np.random.randint(2, size = (num_examples, num_visible))))
+    return np.concatenate((data, np.random.randint(2, size=(num_examples, num_visible))))
 
 pattern = np.array([[1, 0, 0, 1, 0, 1, 0, 1, 0]])
 num_visible = pattern.shape[1]
@@ -31,6 +32,7 @@ print(dataset)
 
 datapath = '../c_lang/dataset/'
 filename = 'make_smolensky_training.bin'
+
 
 file = open(datapath + filename, 'wb')
 file.write(dataset)
