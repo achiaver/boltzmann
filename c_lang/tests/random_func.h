fe670b6f88533191a56c2e08f25d90a61ecbb600
Artur Chiaperini Grover <artur.turbas@gmail.com>
2020-07-20 18:04:30 -0300

    tested pcg random...

---
+++ c_lang/tests/random_func.h
@@ -0,0 +1,11 @@
+#ifndef __RANDOM_FUNC__
+#define __RANDOM_FUNC__
+
+#include <time.h>
+#include "ran3.h"
+
+void init_seed ();
+
+
+#endif
+
--- c_lang/tests/test_ran3.c
+++ c_lang/tests/test_ran3.c
@@ -6,12 +6,13 @@ int main(int argc, char *argv[])
 {
     const time_t timer = time(NULL);
     printf("ctime is %s\n", ctime(&timer));
-    printf("%ld\n", time(NULL));
-    printf("%ld\n",  (-1) * (long int) &timer);
-    printf("%ld\n", timer);
+    
+    printf("time(NULL) - %ld\n", time(NULL));
+    printf("time(NULL) to long int - %ld\n",  (-1) * (long int) &timer);
+    printf("timer - %ld\n", timer);
     int seed = (-1) * (int)timer;
-    printf("%f\n", ran3(&seed));
+    printf("ran3(seed timer) - %f\n", ran3(&seed));
     seed = -42;
-    printf("%f\n", ran3(&seed));
+    printf("ran3(seed fixed) - %f\n", ran3(&seed));
     return 0;
 }
