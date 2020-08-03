fdf2becdc8f19d99a34cbaba8a3a1eafd225bde6
Artur C. Grover <artur.turbas@gmail.com>
2020-08-03 16:00:08 -0300

    ported simulated annealing to james... but results are not good.

--- c_lang/james.c
+++ c_lang/james.c
@@ -4,67 +4,67 @@
 #define DEBUG 1
 #define PERCENT_ERROR_ENERGY 1.E-10
 
-struct layer *
-visible_to_hidden_j (struct layer * visible, struct network * net, double T)
-{
-    struct layer * hidden = layer_create(net->hidden.num_nodes);
-    for (int h = 0 ; h < hidden->num_nodes; h++)
-    {
-        double sum = 0.;
-        for (int v = 0; v < visible->num_nodes; v++)
-        {
-            sum += node_get_activation(visible->nodes, v) * matrix_get(net->weights, v, h);
-        }
-        sum -= 0.7;
-        node_set_nprob(hidden->nodes, h, func_sigmoid(sum, T));
-        if (node_get_nprob(hidden->nodes, h) >= random_0to1())
-        {
-            node_set_activation(hidden->nodes, h, 1.);
-        } else
-        {
-            node_set_activation(hidden->nodes, h, 0.);
-        }
-    }
-    return hidden;
-}
-
-struct layer *
-hidden_to_visible_j (struct layer * hidden, struct network * net, double T)
-{
-    struct layer * visible = layer_create(net->visible.num_nodes);
-    for (int v = 0; v < visible->num_nodes; v++)
-    {
-        double sum = 0.;
-        for (int h = 0; h < hidden->num_nodes; h++)
-        {
-           sum += node_get_activation(hidden->nodes, h) * matrix_get(net->weights, v, h);
-        }
-        sum *= 2.;
-        node_set_nprob(visible->nodes, v, func_sigmoid(sum, T));
-        if (node_get_nprob(visible->nodes, v) >= random_0to1())
-        {
-            node_set_activation(visible->nodes, v, 1.);
-        } else
-        {
-            node_set_activation(visible->nodes, v, -1.);
-        }
-    }
-    return visible;
-} /* end visible_from_hidden */
+//struct layer *
+//visible_to_hidden_j (struct layer * visible, struct network * net, double T)
+//{
+//    struct layer * hidden = layer_create(net->hidden.num_nodes);
+//    for (int h = 0 ; h < hidden->num_nodes; h++)
+//    {
+//        double sum = 0.;
+//        for (int v = 0; v < visible->num_nodes; v++)
+//        {
+//            sum += node_get_activation(visible->nodes, v) * matrix_get(net->weights, v, h);
+//        }
+//        sum += node_get_bias(net->hidden.nodes, h);
+//        node_set_nprob(hidden->nodes, h, func_sigmoid(sum, T));
+//        if (node_get_nprob(hidden->nodes, h) >= random_0to1())
+//        {
+//            node_set_activation(hidden->nodes, h, 1.);
+//        } else
+//        {
+//            node_set_activation(hidden->nodes, h, 0.);
+//        }
+//    }
+//    return hidden;
+//}
+//
+//struct layer *
+//hidden_to_visible_j (struct layer * hidden, struct network * net, double T)
+//{
+//    struct layer * visible = layer_create(net->visible.num_nodes);
+//    for (int v = 0; v < visible->num_nodes; v++)
+//    {
+//        double sum = 0.;
+//        for (int h = 0; h < hidden->num_nodes; h++)
+//        {
+//           sum += node_get_activation(hidden->nodes, h) * matrix_get(net->weights, v, h);
+//        }
+//        sum += node_get_bias(net->visible.nodes, v);
+//        node_set_nprob(visible->nodes, v, func_sigmoid(sum, T));
+//        if (node_get_nprob(visible->nodes, v) >= random_0to1())
+//        {
+//            node_set_activation(visible->nodes, v, 1.);
+//        } else
+//        {
+//            node_set_activation(visible->nodes, v, 0.);
+//        }
+//    }
+//    return visible;
+//} /* end visible_from_hidden */
 
 
 void
-layer_copy_layer (struct layer * lay_1, struct layer * lay_2)
+layer_copy_layer_j (struct layer * lay_1, struct layer * lay_2)
 {
     for (int i = 0; i < lay_1->num_nodes; i++)
     {
-        if (node_get_activation(lay_1->nodes, i) == 0.)
+        if (node_get_activation(lay_1->nodes, i) == -1.)
         {
             double act = random_activation();
             if (act == 1.)
                 node_set_activation(lay_2->nodes, i, 1.);
             else
-                node_set_activation(lay_2->nodes, i, -1.);
+                node_set_activation(lay_2->nodes, i, 0.);
         } else
         {
             node_set_activation(lay_2->nodes, i, node_get_activation(lay_1->nodes, i));
@@ -74,18 +74,15 @@ layer_copy_layer (struct layer * lay_1, struct layer * lay_2)
 
 
 struct layer *
-simulated_annealing (struct network * net, struct layer * input, struct parameters * param)
+simulated_annealing_j (struct network * net, struct layer * input, struct parameters * param)
 {
     int crystalized = 0; // False
     double infinite_energy = (net->hidden.num_nodes + net->visible.num_nodes) * (net->hidden.num_nodes + net->visible.num_nodes) * 1.0E5;
     double last_mean_energy = 0.;
 
-//    struct layer * input_aux = layer_create(input->num_nodes);
-//    struct layer * hidden = layer_create(net->hidden.num_nodes);
-//    layer_copy_layer(input, input_aux);
-    layer_copy_layer(input, &net->visible);
-//    layer_print(input_aux, 0);
-//    printf("\n");
+    layer_copy_layer_j(input, &net->visible);
+    layer_print(&net->visible, 0);
+    printf("\n");
     double n_iterations_mean_energy = param->tries_per_node * net->num_nodes_total;
     double convergence_error = 0.;
 
@@ -97,12 +94,11 @@ simulated_annealing (struct network * net, struct layer * input, struct paramete
         int thermalized = 0; // False
         double last_temp_mean_energy = infinite_energy;
         double mean_energy = 0.;
-        
+
         while ((!thermalized) && (iteration < net->num_nodes_total))
         {
             for (int i_mean = 0; i_mean < param->tries_per_node; i_mean++)
             {
-                
                 // Update hidden units
                 for (int h = 0; h < net->hidden.num_nodes; h++)
                 {
@@ -112,16 +108,16 @@ simulated_annealing (struct network * net, struct layer * input, struct paramete
                         delta_energy = delta_energy + node_get_activation(net->visible.nodes, v) * matrix_get(net->weights, v, h);
                     }
                     delta_energy = delta_energy + node_get_bias(net->hidden.nodes, h);
-                    
+
                     node_set_nprob(net->hidden.nodes, h, func_sigmoid(delta_energy, temp_current));
                     if (node_get_nprob(net->hidden.nodes, h) >= random_0to1())
                     {
                         node_set_activation(net->hidden.nodes, h, 1.);
                     } else
                     {
-                        node_set_activation(net->hidden.nodes, h, -1.);
+                        node_set_activation(net->hidden.nodes, h, 0.);
                     }
-                    mean_energy = mean_energy + func_energy(net->weights, &net->visible, &net->hidden);    
+                    mean_energy = mean_energy + func_energy(net->weights, &net->visible, &net->hidden);
                 }
                 // Update visible units
                 for (int v = 0; v < net->visible.num_nodes; v++)
@@ -132,14 +128,14 @@ simulated_annealing (struct network * net, struct layer * input, struct paramete
                         delta_energy = delta_energy + node_get_activation(net->hidden.nodes, h) * matrix_get(net->weights, v, h);
                     }
                     delta_energy = delta_energy + node_get_bias(net->visible.nodes,v);
-                    
+
                     node_set_nprob(net->visible.nodes, v, func_sigmoid(delta_energy, temp_current));
                     if (node_get_nprob(net->visible.nodes, v) >= random_0to1())
                     {
                         node_set_activation(net->visible.nodes, v, 1.);
                     } else
                     {
-                        node_set_activation(net->visible.nodes, v, -1.);
+                        node_set_activation(net->visible.nodes, v, 0.);
                     }
                     mean_energy = mean_energy + func_energy(net->weights, &net->visible, &net->hidden);
                 }
@@ -166,54 +162,11 @@ simulated_annealing (struct network * net, struct layer * input, struct paramete
             temp_current = param->temp_update * temp_current;
         }
 
-        printf("T - %f\n", temp_current);
+ //       printf("T - %f\n", temp_current);
     }
 //    layer_delete(hidden);
     return &net->visible;
 } /* end simulated_annealing*/
-
-
-
-//void
-//layer_copy_layer (struct layer * lay_1, struct layer * lay_2)
-//{
-//    for (int i = 0; i < lay_1->num_nodes; i++)
-//    {
-//        if (node_get_activation(lay_1->nodes, i) == -1.)
-//        {
-//            node_set_activation(lay_2->nodes, i, (double)rand() / (double)RAND_MAX);
-//        } else
-//        {
-//            node_set_activation(lay_2->nodes, i, node_get_activation(lay_1->nodes, i));
-//        }
-//    }
-//}
-//
-//
-//struct layer *
-//simulated_annealing (struct network * net, struct layer * input)
-//{
-//    struct layer * lay = layer_create(input->num_nodes);
-//    struct layer * hidden = layer_create(net->hidden.num_nodes);
-//    layer_copy_layer(input, lay);
-//    layer_print(lay, 0);
-//    printf("\n");
-//
-//    double T = 100.0;
-//    while (T >= 1)
-//    {
-//        hidden = hidden_from_visible(net, lay, T);
-//        layer_print(hidden, 0);
-//        printf("\n");
-//        lay = visible_from_hidden(net, hidden, T); 
-//        layer_print(lay, 0);
-//        printf("\n");
-//
-//        T = T * 0.95;
-//    }
-//    layer_delete(hidden, 0);
-//    return lay;
-//} /* end simulated_annealing*/
 
 
 int
@@ -244,22 +197,28 @@ main(int argc, char *argv[])
     dataset_dump(dataset);
 
     struct parameters * param = parameters_create();
-    param->num_layers = 2;
-    param->num_visible = 6;
-    param->num_hidden = 3;
-
+    if (param)
+    {
+        param->temp_start = 5.995;
+        param->temp_end = 0.001;
+        param->temp_update = 0.95;
+        param->tries_per_node = 20;
+        param->num_layers = 2;
+        param->num_visible = 6;
+        param->num_hidden = 3;
+        param->dataset_rows = 12;
+        param->dataset_cols = 6;
+        param->epsilonw = 0.01;
+        param->epsilonvb = 0.01;
+        param->epsilonhb = 0.01;
+        param->maxepochs = 1000;
+    }
     printf("Creating a RBM ... \n");
     printf("Setting number of visible nodes = %zu \n", param->num_visible);
     printf("Setting number of hidden nodes = %zu \n\n", param->num_hidden);
 
     struct network * net = network_create(param);
 
-    param->dataset_rows = 12;
-    param->dataset_cols = 6;
-    param->epsilonw = 0.01;
-    param->epsilonvb = 0.01;
-    param->epsilonhb = 0.01;
-    param->maxepochs = 1000;
 
     printf("Training RBM using CD1 algorithm \n");
     printf("Setting learning rate (weights and biases) = %f \n", param->epsilonw);
@@ -271,7 +230,8 @@ main(int argc, char *argv[])
     printf("Trained machine's weights and biases are: \n");
     network_dump(net, 0, 1, 1);
 
-    printf("\nUsing trained machine - NOT Simulated Annealing -\n");
+    printf("\n- Using the network -\n");
+    printf("- NOT Simulated Annealing -\n");
 
     struct layer * visible = layer_create(net->visible.num_nodes);
     layer_copy_from_array(visible, dataset, 0);
@@ -290,28 +250,28 @@ main(int argc, char *argv[])
     layer_print(visible_computed, 1);
     printf("\n");
 
-//    printf("- SIMULATED ANNEALING TEST -\n");
-//    // Select a test example, where activation as -1 means that these unit will be randomly initialized.
+    printf("- SIMULATED ANNEALING TEST -\n");
+    // Select a test example, where activation as -1 means that these unit will be randomly initialized.
 //    double test_example[1][6] = {{1,-1,0,-1,0,0}};
 //    struct matrix * test_m = dataset_example(1, 6, test_example);
-//    struct layer * test_l = layer_create(net->visible.num_nodes);
-//    layer_copy_from_array(test_l, test_m, 0);
-//    layer_print(test_l, 0);
-//    printf("\n");
+    struct layer * test_l = layer_create(net->visible.num_nodes);
+    layer_copy_from_array(test_l, dataset, 1);
+    layer_print(test_l, 0);
+    printf("\n");
 //    printf("delete test_m\n");
 //    matrix_destroy(test_m);
-//
-//    struct layer * simu = simulated_annealing(net, test_l);
-//    printf("\n");
-//    layer_print(simu, 0);
-//    printf("\n");
-//
-//    printf("\n\n\n");
-//    printf("delete simu\n");
-//    layer_delete(simu, 1);
-//
-//    printf("delete test_l\n");
-//    layer_delete(test_l, 0);
+
+    struct layer * simu = simulated_annealing_j(net, test_l, param);
+    printf("\n");
+    layer_print(simu, 0);
+    printf("\n");
+
+    printf("\n\n\n");
+    printf("delete simu\n");
+    layer_delete(simu);
+
+    printf("delete test_l\n");
+    layer_delete(test_l);
 
     printf("delete dataset\n");
     matrix_destroy(dataset);
